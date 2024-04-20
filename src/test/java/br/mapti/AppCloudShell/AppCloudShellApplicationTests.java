25b0b59696730f1d9d0126baa81396e3bd467a95
Michel Pereira <pereira.michel@gmail.com>
2024-04-20 00:20:01 -0300

    Create project

---
+++ src/test/java/br/mapti/AppCloudShell/AppCloudShellApplicationTests.java
@@ -0,0 +1,13 @@
+package br.mapti.AppCloudShell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppCloudShellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
